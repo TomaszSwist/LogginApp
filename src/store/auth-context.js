464e71646b0f072950e0c418d320545e7a261f22
Tomasz Świst <kontakt@tomaszswist.pl>
2023-02-28 19:11:13 +0100

    useContext usage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const AuthContext = React.createContext({ isLoggedIn: false })
+const AuthContext = React.createContext({ isLoggedIn: false, onLogout: () => {} })
 
 export default AuthContext
